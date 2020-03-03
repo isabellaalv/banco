fe614d7a7d485dd0f1bc2bfa8d29b294636fdc07
Isabella Alves <50323919804@info.local>
2020-03-03 16:05:50 -0300

    script e modelagem

--- InLock_BD_DML_02.sql
+++ InLock_BD_DML_02.sql
@@ -11,7 +11,7 @@ VALUES ('Blizzard') ,
 SELECT * FROM Estudios;
 
 INSERT INTO Usuarios (Email, Senha, TipoUsuarioId)
-VALUES ('admi@adim.com' , 'admin' , 1),
+VALUES ('admin@admin.com' , 'admin' , 1),
 ('cliente@cliente.com', 'cliente', 2);
 
 
@@ -20,4 +20,8 @@ VALUES ('Diablo 3', 'é um jogo que contém bastante ação e é
 viciante, seja você um novato ou um fã', '2012/05/15', 99.00, 1),
 ('Red Dead Redemption II', 'jogo eletrônico de ação-aventura western',
 '2018/10/26',120.00, 2);
+
+UPDATE Usuarios
+SET Email = 'admin@admin.com'
+WHERE Id = 1;
 
